429149dc3a9035e2eb4478c21202ed4109b581cc
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-17 21:16:03 -0500

    feat: finish mobile mockup

--- src/components/index/DonateSection.tsx
+++ src/components/index/DonateSection.tsx
@@ -1,7 +1,19 @@
-import { DonateSectionImage } from "../../styles/index/MainPage";
+import { AnotherDarkMagician, DonateButton, DonateImageText, DonateSectionImage, DonationDescription, DonationTextWrapper, DonationTitle } from "../../styles/index/MainPage";
 
 const DonateSection = () => (
     <DonateSectionImage background="/assets/GirlMagicianChocoBlue.png">
+        <DonateImageText>
+            <AnotherDarkMagician src="/assets/AnotherDarkMagician.png" />
+            <DonationTextWrapper>
+                <DonationTitle>¡Apoyanos para seguir desarrollando!</DonationTitle>
+                <DonationDescription>
+                    Somos 2 fanáticos de Yugioh! que queremos crear nuevas
+                    herramientas para mejorar nuestra experiencia de juego, tu apoyo nos ayudará a que 
+                    sigamos inventando herramientas funcionales y divertidas para la comunidad. 
+                </DonationDescription>
+                <DonateButton>Yo si valoro su esfuerzo XD</DonateButton>
+            </DonationTextWrapper>
+        </DonateImageText>
     </DonateSectionImage>
 );
 
---
+++ src/styles/footer/Footer.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+interface FooterProps {
+    background?: string;
+}
+
+export const FooterStyle = styled.div`
+    padding: 30px 10px 30px 0;
+    background: ${({background}: FooterProps) => `url(${background}) no-repeat;`};
+    height: 214px;
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+`;
+
+export const WitchcraftTextWrapper = styled.div`
+    display: flex;
+    height: 100%;
+    width: 100%;
+`;
+
+export const WitchcraftImage = styled.img`
+
+`;
+
+export const FooterTextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: auto 0 5px 18%;
+`;
+
+export const FooterTitle = styled.p`
+    font-family: "Dharma-Regular";
+    font-size: 56px;
+    margin: 0 0 5px 0;
+    text-align: center;
+`;
+
+export const FooterSubtitle = styled.p`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 12px;
+    text-align: center;
+`;
