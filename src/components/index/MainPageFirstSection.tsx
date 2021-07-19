df63ea432d136e3cbe4d6ac62ad7985bf722007c
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-18 23:24:25 -0500

    feat: add new rediretion button

--- src/components/index/MainPageFirstSection.tsx
+++ src/components/index/MainPageFirstSection.tsx
@@ -1,25 +1,42 @@
-import { CardFrameVertical, DarkMagician, DarkMagicianGirl, MainImage, MainImageDesktop, MainTitle, MainTitleDesktop, MainWrapperDesktop, StartButton, StartButtonDesktop, Subtitle, SubtitleDesktop } from "../../styles/index/MainPage";
+import Link from "next/link";
+import { LinkTo } from "../../styles/common/Link";
+import {
+  CardFrameVertical,
+  DarkMagician,
+  DarkMagicianGirl,
+  MainImage,
+  MainImageDesktop,
+  MainTitle,
+  MainTitleDesktop,
+  MainWrapperDesktop,
+  StartButton,
+  StartButtonDesktop,
+  Subtitle,
+  SubtitleDesktop,
+} from "../../styles/index/MainPage";
 
 const MainPageFirstSection = () => (
-    <>
-        <MainWrapperDesktop>
-            <DarkMagician />
-            <CardFrameVertical />
-                <MainImageDesktop background="radial-gradient(50% 50% at 50% 48.99%, rgba(0, 0, 0, 0) 5.73%, rgba(3, 41, 96, 0.5) 100%), url(/assets/BGCards.png)">
-                    <MainTitleDesktop>YugiCards!</MainTitleDesktop>
-                    <SubtitleDesktop>¡Cotizar tu deck nunca 
-                    fue tan facil!</SubtitleDesktop>
-                    <StartButtonDesktop>¡Comenzar!</StartButtonDesktop>
-                </MainImageDesktop>
-        </MainWrapperDesktop>
-        <MainImage background="radial-gradient(50% 50% at 50% 48.99%, rgba(0, 0, 0, 0) 5.73%, rgba(3, 41, 96, 0.5) 100%), url(/assets/BGCards.png)">
-            <MainTitle>YugiCards!</MainTitle>
-            <Subtitle>¡Cotizar tu deck nunca 
-            fue tan facil!</Subtitle>
-            <StartButton>¡Comenzar!</StartButton>
-            <DarkMagicianGirl src="/assets/darkMagicianGirl.png" />
-        </MainImage>
-    </>
+  <>
+    <MainWrapperDesktop>
+      <DarkMagician />
+      <CardFrameVertical />
+      <MainImageDesktop background="radial-gradient(50% 50% at 50% 48.99%, rgba(0, 0, 0, 0) 5.73%, rgba(3, 41, 96, 0.5) 100%), url(/assets/BGCards.png)">
+        <MainTitleDesktop>YugiCards!</MainTitleDesktop>
+        <SubtitleDesktop>¡Cotizar tu deck nunca fue tan facil!</SubtitleDesktop>
+        <Link href="/deckPrice">
+          <LinkTo>
+            <StartButtonDesktop>¡Comenzar!</StartButtonDesktop>
+          </LinkTo>
+        </Link>
+      </MainImageDesktop>
+    </MainWrapperDesktop>
+    <MainImage background="radial-gradient(50% 50% at 50% 48.99%, rgba(0, 0, 0, 0) 5.73%, rgba(3, 41, 96, 0.5) 100%), url(/assets/BGCards.png)">
+      <MainTitle>YugiCards!</MainTitle>
+      <Subtitle>¡Cotizar tu deck nunca fue tan facil!</Subtitle>
+      <StartButton>¡Comenzar!</StartButton>
+      <DarkMagicianGirl src="/assets/darkMagicianGirl.png" />
+    </MainImage>
+  </>
 );
 
 export default MainPageFirstSection;
