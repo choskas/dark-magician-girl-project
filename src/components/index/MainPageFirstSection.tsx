cc12b743b18c8b253818326fe0c122e1208b30c2
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-18 23:35:19 -0500

    fix: redirection on button mobile

--- src/components/index/MainPageFirstSection.tsx
+++ src/components/index/MainPageFirstSection.tsx
@@ -33,7 +33,11 @@ const MainPageFirstSection = () => (
     <MainImage background="radial-gradient(50% 50% at 50% 48.99%, rgba(0, 0, 0, 0) 5.73%, rgba(3, 41, 96, 0.5) 100%), url(/assets/BGCards.png)">
       <MainTitle>YugiCards!</MainTitle>
       <Subtitle>¡Cotizar tu deck nunca fue tan facil!</Subtitle>
-      <StartButton>¡Comenzar!</StartButton>
+      <Link href="/deckPrice">
+        <LinkTo>
+          <StartButton>¡Comenzar!</StartButton>
+        </LinkTo>
+      </Link>
       <DarkMagicianGirl src="/assets/darkMagicianGirl.png" />
     </MainImage>
   </>
