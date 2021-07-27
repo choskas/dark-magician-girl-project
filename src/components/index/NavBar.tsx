b52e6b0f305a5534c9013e922924c389b4ee253b
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-27 09:20:31 -0500

    add

--- src/components/index/NavBar.tsx
+++ src/components/index/NavBar.tsx
@@ -37,13 +37,6 @@ const NavBar = () => {
         <CollapseOption onClick={() => router.push('/deckPrice')}> Cotizar deck </CollapseOption>
       </>
     );
-    const getProfile = async () => {
-      const user = await axios.get('https://yugicardsbackend.herokuapp.com/profile')
-      console.log(user)
-    }
-    useEffect(() => {
-      getProfile();
-    })
   return (
     <Wrapper>
       <Link href="/">
