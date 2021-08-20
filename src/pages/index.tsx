f90f0d06f51dbd5e76809ca99f949f9abd94e2fa
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-08-20 13:54:24 -0500

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import WhatToDoSection from "../components/index/WhatToDoSection";
 import { CardFrame } from "../styles/index/MainPage";
 
 const HomePage = () => {
+  
   return (
     <>
       <NavBar />
