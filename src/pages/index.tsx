c6c77727726b5e1295db8e05ef6a4e775eaee7ba
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-08-20 12:55:47 -0500

    delete console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import WhatToDoSection from "../components/index/WhatToDoSection";
 import { CardFrame } from "../styles/index/MainPage";
 
 const HomePage = () => {
-  console.log('this is dev')
   return (
     <>
       <NavBar />
